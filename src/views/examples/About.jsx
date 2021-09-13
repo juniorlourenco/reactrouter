8e04ade3d5b52eab16f31143e8754889b94bcf79
Junior Lourenço <juniorminaj101@gmail.com>
2021-09-13 09:37:18 -0400

    route with params

--- src/views/examples/About.jsx
+++ src/views/examples/About.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const About = props => (
     <div className="About">
         <h1>Sobre</h1>
-        <p>O nosso sistema foi criado no Curso de React da Cod3r</p>
+        <h2>O nosso sistema foi criado no Curso de React da Cod3r</h2>
     </div>
 )
 
